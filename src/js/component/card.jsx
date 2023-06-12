a0dfc36727f1b8017ce2312a3347b24150cf9ec4
Shaleena Evans <123431776+eshaleena@users.noreply.github.com>
2023-06-12 20:46:49 +0000

    second commit

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -3,7 +3,23 @@ import React from "react"
 export const Card = () => {
     return (
         <div className="col">
+        <div className="row">
+  <div className="col-lg-6">
+    <div className="card">
+      <div className="card-body">
+      <img class="card-img-top" src="https://via.placeholder.com/500x325" alt="Card image cap"/>
+        <p className="card-text">
+             <h4>Card Title</h4>
+             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, similique.
+           </p>
+      </div>
+    </div>
+  </div>
 
+</div>
+<div className="bg-light">
+<a href="#" className="btn btn-primary btn-sm">Find Out More!</a></div>
         </div>
+
     )
 }
--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -2,8 +2,10 @@ import React from "react"
 
 export const Jumbotron = () => {
     return (
-        <div>
-            <h1>A Warm Welcome</h1>
+        <div className="heading">
+            <h1 >A Warm Welcome!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi obcaecati eos corporis quo dolor animi, nesciunt veritatis quibusdam culpa dignissimos ut? Et earum quisquam perferendis nulla eum, excepturi nemo amet!</p>
+            <button className="button">Call to action!</button>
         </div>
     )
 }
